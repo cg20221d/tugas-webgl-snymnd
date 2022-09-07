b4714f49b8895cffb327328bb17d1cb4a8631d04
Muhammad Yunus <Yunusenvire@gmail.com>
2022-09-07 15:23:45 +0700

    feat: make shader 1.0

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-const main =()=>{
-    const canvas = document.querySelector('#c');
+const main = () =>{
+    const canvas = document.querySelector('#kanvas');
     const gl = canvas.getContext('webgl');
-    if(!gl){
-        return;
-    }
-    gl.clearColor(0,0,0,0);
+
+    // vertex shader
+    const vertexShaderCode = 
+    `void main() {
+
+    }`
+    const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject); //sampai sini sudah menjadi .o
+
+    // fragmen shader
+    const fragmenShaderCode = 
+    `void main(){
+
+    }`
+    const fragmenShaaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmenShaaderObject, fragmenShaderCode);
+    gl.compileShader(fragmenShaaderObject); //sampai sini sudah menjadi .o
+
+    // shader program
+    const shaderProgram = gl.createProgram(); //(.exe)
+    gl.attachShader(shaderProgram, vertexShaderObject);
+    gl.attachShader(shaderProgram, fragmenShaaderObject);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    // drawing
+    gl.clearColor(0.0, 0.0, 0.0, 1.0); //(R G B A)
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // animasi
+    let a = 2;
+    let loop = () =>{
+        gl.clearColor(0.0, 1.0, 0.0, 1.0); //(R G B A)
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+
+
 }
