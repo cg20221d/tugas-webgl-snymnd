c91e392351dac4d97e3919a8d50a06e8e07bad59
Muhammad Yunus <Yunusenvire@gmail.com>
2022-09-14 14:52:00 +0700

    feat: add shader code

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ const main = () =>{
     // vertex shader
     const vertexShaderCode = 
     `void main() {
-
+        float x = 0.0;
+        float y = 0.0;
+        gl_PointSize = 100.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
     }`
     const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
     gl.compileShader(vertexShaderObject); //sampai sini sudah menjadi .o
 
     // fragmen shader
-    const fragmenShaderCode = 
-    `void main(){
+    const fragmenShaderCode = `
+    precision mediump float;
+    void main(){
+
+        float r = 1.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
+
 
     }`
     const fragmenShaaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -28,17 +38,8 @@ const main = () =>{
     gl.useProgram(shaderProgram);
 
     // drawing
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); //(R G B A)
+    gl.clearColor(0.0, 1.0, 1.0, 1.0); //(R G B A)
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // animasi
-    let a = 2;
-    let loop = () =>{
-        gl.clearColor(0.0, 1.0, 0.0, 1.0); //(R G B A)
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        requestAnimationFrame(loop);
-    }
-    requestAnimationFrame(loop);
-
-
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
