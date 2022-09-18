af94938a516cbf9cc5e4903150812416c2136501
Muhammad Yunus <Yunusenvire@gmail.com>
2022-09-18 22:56:45 +0700

    feat: add vertices form 1

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 const main = () =>{
     const canvas = document.querySelector('#kanvas');
     const gl = canvas.getContext('webgl');
-
-    const vertices = [
-        //vertices form number 7
-        -0.9, 0.9, 
-        -0.55, 0.9,
-
-        -0.8, 0.5,
-        -0.9, 0.5,
-
-        -0.7, 0.82,
-        -0.9, 0.82,
-    ];
     
     const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
 
     // vertex shader
     const vertexShaderCode = 
@@ -52,16 +37,57 @@ const main = () =>{
     gl.attachShader(shaderProgram, fragmenShaaderObject);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
-
-    // bind attribute : told gpu how to collect position value from buffer to  every vertex that processing 
-    const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
-
-
-    // drawing
+    
     gl.clearColor(0.0, 1.0, 1.0, 1.0); //(R G B A)
     gl.clear(gl.COLOR_BUFFER_BIT);
+    // bind attribute : told gpu how to collect position value from buffer to  every vertex that processing 
+    const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
+    
+    
+    // drawing function
+    const drawing = (vertices, start=0, end) =>{        
+        // bind buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(aPosition);
+        gl.drawArrays(gl.LINE_LOOP, start, end);
+    }
+    
 
-    gl.drawArrays(gl.LINE_LOOP, 0, 6);
+    //f form 7
+    const vertices7 = [
+        //vertices form number 7
+        -0.9, 0.9, 
+        -0.55, 0.9,
+
+        -0.8, 0.5,
+        -0.9, 0.5,
+
+        -0.7, 0.82,
+        -0.9, 0.82,
+    ];
+    drawing(vertices7, 0, 6); 
+
+    // form 1
+    const vertices1 = [
+        //vertices form number 7
+        -0.4, 0.9, 
+        -0.3, 0.9,
+        
+        -0.3, 0.58,
+        -0.25, 0.58,
+        
+        -0.25, 0.5,
+        -0.45, 0.5,
+
+        -0.45, 0.58,
+        -0.4, 0.58,
+
+        -0.4, 0.8,
+        -0.45, 0.8,
+
+        -0.45, 0.85,
+    ];
+    drawing(vertices1, 0, 11); 
 }
