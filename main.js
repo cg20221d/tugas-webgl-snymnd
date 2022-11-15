796e9607b1f43e140e6d1906870f3f7e5377ba6f
Muhammad Yunus <Yunusenvire@gmail.com>
2022-11-16 00:29:52 +0700

    feat: make alphabet 3D

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const main = () =>{
     const canvas = document.querySelector('#kanvas');
     const gl = canvas.getContext('webgl');
     
-    // const buffer = gl.createBuffer();
-    // const indexBuffer = gl.createBuffer();
-
     // vertex shader
     const vertexShaderCode = 
     `
@@ -46,10 +43,6 @@ const main = () =>{
     var theta = 0.0;
     var freeze = false;
 
-
-    
-    // variable pointer ke GLSL
-    // var uModel = gl.getUniformLocation(shaderProgram, "uModel");
     // View
     var cameraX = 0.0;
     var cameraZ = 7.5;
@@ -57,22 +50,15 @@ const main = () =>{
     // var uView = gl.getUniformLocation(shaderProgram, "uView");
     glMatrix.mat4.lookAt(
         view,
-        [cameraX, 0.0, cameraZ],    // the location of the eye or the camera
+        [cameraX, 3, cameraZ],    // the location of the eye or the camera
         [cameraX, 0.0, 0],        // the point where the camera look at
         [0.0, 1.0, 0.0]
     );
-    // Projection
-    // var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
     var perspective = glMatrix.mat4.create();
     glMatrix.mat4.perspective(perspective, Math.PI/2.4, 1.0, 0.5, 50.0);
-
-    // bind attribute : told gpu how to collect position value from buffer to  every vertex that processing 
-    // const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
-    // const aColor = gl.getAttribLocation(shaderProgram, 'aColor');
-        
     
     // drawing function
-    const drawing = (vertices, indices, start=0, end, glType=gl.LINE_LOOP) =>{ 
+    const drawing = (vertices, indices, glType=gl.LINE_LOOP) =>{ 
         const buffer = gl.createBuffer();
         const indexBuffer = gl.createBuffer();
 
@@ -91,9 +77,7 @@ const main = () =>{
             0 * Float32Array.BYTES_PER_ELEMENT
         );
         gl.enableVertexAttribArray(aPosition);
-        
-        // gl.drawArrays(glType, start, end);
-        
+
         gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
             6 * Float32Array.BYTES_PER_ELEMENT, 
             3 * Float32Array.BYTES_PER_ELEMENT 
@@ -105,7 +89,6 @@ const main = () =>{
     
     const frameWidth = 12.4 //in mat4 unit
     var horizontalSpeed = 0.171; //nrp
-    var verticalSpeed = 0.0;
     var horizontalDelta = 0.0;
     var verticalDelta = 0.0;
 
@@ -114,8 +97,6 @@ const main = () =>{
     /// animating object '7'
     const animate7 = () =>{
         var model = glMatrix.mat4.create();
-
-
 
         if (horizontalDelta >= (frameWidth/2) || horizontalDelta <= (-frameWidth/2+1)) {
             horizontalSpeed = horizontalSpeed * -1;
@@ -129,13 +110,14 @@ const main = () =>{
         gl.uniformMatrix4fv(uModel,false, model);
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
-        drawing(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
+        drawing(objects[0].vertices, objects[0].indices, objects[0].type);
     }
     
     const animate1 = () =>{
         var model = glMatrix.mat4.create();
 
-        if (scaleDelta >= 1 || scaleDelta <= -0.5) { //scale 2x lipat (maju 1 unit z), scale 0.5x kalo lipat (mundur 0.5 unit z)
+        //scale 2x lipat (maju 1 unit z), scale 0.5x kalo lipat (mundur 0.5 unit z)
+        if (scaleDelta >= 1 || scaleDelta <= -0.5) { 
             scaleSpeed = scaleSpeed * -1;
         }
         scaleDelta += scaleSpeed;
@@ -147,63 +129,40 @@ const main = () =>{
         gl.uniformMatrix4fv(uModel,false, model);
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
-        drawing(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
+        drawing(objects[1].vertices, objects[1].indices, objects[1].type);
     }
     
     const render = () => {  
         gl.clearColor(0.0, 1.0, 1.0, 1.0); //(R G B A)
         // Clear the canvas AND the depth buffer.
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  
-        
-        
-        // glMatrix.mat4.translate(model, model, [horizontalDelta, verticalDelta, 0.0]);
-        // drawing(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
-        
         var model = glMatrix.mat4.create();
+
+        theta += 0.01;
+        glMatrix.mat4.rotateY(model, model, theta *.7, [0.0, 1.0, 0.0]);
+        // glMatrix.mat4.translate(model, model, [0, 0, 0.0]);
+        
         var uModel = gl.getUniformLocation(shaderProgram, "uModel");
         var uView = gl.getUniformLocation(shaderProgram, "uView");
-        var uProjection = gl.getUniformLocation(shaderProgram, "uProjection"); 
+        var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
         gl.uniformMatrix4fv(uModel,false, model);
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
+        drawing(objects[3].vertices, objects[3].indices, objects[3].type);
+        drawing(objects[2].vertices, objects[2].indices, objects[2].type);
+        
+        
+
+        
         animate7();
         animate1();
+        
+        
+        
         requestAnimationFrame(render);
     }
 
     requestAnimationFrame(render);
-    
-    // const render = () => {    
-    //     // gl.clearColor(0.0, 1.0, 1.0, 1.0); //(R G B A)
-    //     // gl.clear(gl.COLOR_BUFFER_BIT);
-    //     // const model = glMatrix.mat4.create();
-
-    //     // if (!freeze) {
-    //     //     theta += 0.1;
-    //     // }
-    //     // verticalDelta -= verticalSpeed;
-    //     // var model = glMatrix.mat4.create();
-    //     // glMatrix.mat4.rotateY(
-    //         //     model,
-    //         //     model,
-    //         //     [1]
-    //         //     // theta * Math.PI / 100
-    //         // ) 
-    //     // animate7();
-    //     const model = glMatrix.mat4.create();
-            
-    //     gl.uniformMatrix4fv(uModel,false, model);
-    //     gl.uniformMatrix4fv(uView, false, view);
-    //     gl.uniformMatrix4fv(uProjection, false, perspective);
-    //     // objects.map((object) => {
-    //     //     drawing(object.vertices, object.indices, 0, object.length, object.type);
-    //     // });
-    //     // gl.drawArrays(gltype, start, end);
-    //     drawing(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
-    //     requestAnimationFrame(render);
-    // }
-
-    // requestAnimationFrame(render);
     
     //f form 7
     const vertices7 = [
@@ -325,53 +284,273 @@ const main = () =>{
         30, 31, 32, 32, 33, 34, 34, 35, 36, 36, 37, 
         38, 38, 39, 40, 41, 42, 43
     ];
-    // drawing(vertices1, 0, 11); 
     
+    // form u
+    // const verticesU = [
+    //     //vertices form number U
+    //     -0.9, 0.4, //a
+    //     -0.8, 0.3, //b
+    //     -0.9, 0.0, //c
+        
+    //     -0.8, 0.0, //bcd
+    //     -0.8, -0.1, //cde
+    //     -0.6, 0.0, //def
+        
+    //     -0.6, -0.1, //efg
+    //     -0.5, 0.0, //fgh
+    //     -0.6, 0.3, //ghi
+    //     -0.5, 0.4, //hij
+    // ];
     
     // form u
     const verticesU = [
         //vertices form number U
-        -0.9, 0.4, //a
-        -0.8, 0.3, //b
-        -0.9, 0.0, //c
-        
-        -0.8, 0.0, //bcd
-        -0.8, -0.1, //cde
-        -0.6, 0.0, //def
-        
-        -0.6, -0.1, //efg
-        -0.5, 0.0, //fgh
-        -0.6, 0.3, //ghi
-        -0.5, 0.4, //hij
-    ];
-    // drawing(verticesU, 0, 10, gl.TRIANGLE_STRIP); 
+        //back-side
+        -0.9, 0.4, 0.0,     1,0,1,//a
+        -0.8, 0.3, 0.0,     1,0,1, //b
+        -0.9, 0.0, 0.0,     1,0,1,//c
     
+        -0.8, 0.0, 0.0,     1,0,1,//bcd
+        -0.8, -0.1,0.0,     1,0,1, //cde
+        -0.6, 0.0, 0.0,     1,0,1,//def
+     
+        -0.6, -0.1,0.0,     1,0,1,//efg
+        
+        -0.5, 0.0, 0.0,     1,0,1,//fgh
+        -0.6, 0.3, 0.0,     1,0,1,//ghi
+        -0.5, 0.4, 0.0,     1,0,1,//hij
+
+        //side face
+        //in-top
+        -0.5, 0.4, 0.0,     0,0,1,//a
+        -0.5, 0.4, 0.1,     0,0,1,//b
+        -0.6, 0.3, 0.0,     0,0,1, //c
+        -0.6, 0.3, 0.1,     0,0,1,//d
+
+        //in-side
+        -0.6, 0.3, 0.0,     0,0,1,//c
+        -0.6, 0.3, 0.1,     0,0,1,//d
+        -0.6, 0.0, 0.0,     0,0,1,//def
+        -0.6, 0.0, 0.1,     0,0,1,//def
+
+        //in-bottom
+        -0.6, 0.0, 0.0,     0,0,1,//def
+        -0.6, 0.0, 0.1,     0,0,1,//def
+        -0.8, 0.0, 0.0,     0,0,1,//bcd
+        -0.8, 0.0, 0.1,     0,0,1,//bcd
+
+        //in-side
+        -0.8, 0.0, 0.0,     0,0,1,//bcd
+        -0.8, 0.0, 0.1,     0,0,1,//bcd
+        -0.8, 0.3, 0.0,     0,0,1,//c
+        -0.8, 0.3, 0.1,     0,0,1,//d
+
+        //in-top
+        -0.8, 0.3, 0.0,     0,0,1,//c
+        -0.8, 0.3, 0.1,     0,0,1,//d
+        -0.9, 0.4, 0.0,     0,0,1,//a
+        -0.9, 0.4, 0.1,     0,0,1,//b
+
+        //out-side
+        -0.9, 0.4, 0.0,     0,0,1,//a
+        -0.9, 0.4, 0.1,     0,0,1,//b
+        -0.9, 0.0, 0.0,     0,0,1,//a
+        -0.9, 0.0, 0.1,     0,0,1,//b
+
+        //out-corner
+        -0.9, 0.0, 0.0,     0,0,1,//a
+        -0.9, 0.0, 0.1,     0,0,1,//b
+        -0.8, -0.1,0.0,     0,0,1,//a
+        -0.8, -0.1,0.1,     0,0,1,//b
+
+        //out-bottom
+        -0.8, -0.1,0.0,     0,0,1,//a
+        -0.8, -0.1,0.1,     0,0,1,//b
+        -0.6, -0.1,0.0,     0,0,1,//a
+        -0.6, -0.1,0.1,     0,0,1,//b
+
+        //out-corner
+        -0.6, -0.1,0.0,     0,0,1,//a
+        -0.6, -0.1,0.1,     0,0,1,//b
+        -0.5, 0.0, 0.0,     0,0,1,//a
+        -0.5, 0.0, 0.1,     0,0,1,//b
+
+        //out-side
+        -0.5, 0.0, 0.0,     0,0,1,//a
+        -0.5, 0.0, 0.1,     0,0,1,//b
+        -0.5, 0.4, 0.0,     0,0,1,//a
+        -0.5, 0.4, 0.1,     0,0,1,//b
+
+        //front-side
+        -0.5, 0.4, 0.1,     1,0,1,//hij
+        -0.6, 0.3, 0.1,     1,0,1,//ghi
+        -0.5, 0.0, 0.1,     1,0,1,//fgh
+        -0.6, -0.1,0.1,     1,0,1,//efg
+        -0.6, 0.0, 0.1,     1,0,1,//def
+        -0.8, -0.1,0.1,     1,0,1, //cde
+        -0.8, 0.0, 0.1,     1,0,1,//bcd
+        -0.9, 0.0, 0.1,     1,0,1,//c
+        -0.8, 0.3, 0.1,     1,0,1, //b
+        -0.9, 0.4, 0.1,     1,0,1,//a
+    
+     
+        
+
+
+
+
+
+    ];
+    const indicesU = [
+        0, 1, 2, 
+        3, 4, 5, 
+        6, 7, 8, 
+        9, 
+        10, 11, 12, 13,
+        14, 15, 16, 17,
+        18, 19, 20, 21,
+        22, 23, 24, 25,
+        26, 27, 28, 29,
+        30, 31, 32, 33,
+        34, 35, 36, 37,
+        38, 39, 40, 41,
+        42, 43, 44, 45,
+        46, 47, 48, 49,
+        50, 51, 52, 53, 54, 
+        55, 56, 57, 58,
+        59
+    ];
+ 
+
     // form S
     const verticesS = [
         //vertices form number S
-        -0.1, 0.4, //a
-        -0.0, 0.3, //b
-        -0.3, 0.4, //c
+        //back-face
+        -0.1, 0.4, 0.0,     1,0,1,//a
+        -0.0, 0.3, 0.0,     1,0,1,//b
+        -0.3, 0.4, 0.0,     1,0,1,//c
         
-        -0.3, 0.3, //bcd
-        -0.4, 0.3, //cde
-        -0.3, 0.2, //eff
+        -0.3, 0.3, 0.0,     1,0,1,//bcd
+        -0.4, 0.3, 0.0,     1,0,1,//cde
+        -0.3, 0.2, 0.0,     1,0,1,//eff
         
-        -0.4, 0.2, //deg
-        -0.3, 0.1, //fgh
-        -0.1, 0.2, //ghi
+        -0.4, 0.2, 0.0,     1,0,1,//deg
+        -0.3, 0.1, 0.0,     1,0,1,//fgh
+        -0.1, 0.2, 0.0,     1,0,1,//ghi
         
-        -0.1, 0.1, //hij
-        -0.0, 0.1, //ijk
-        -0.1, 0.0, //jkl
+        -0.1, 0.1, 0.0,     1,0,1,//hij
+        -0.0, 0.1, 0.0,     1,0,1,//ijk
+        -0.1, 0.0, 0.0,     1,0,1,//jkl
         
-        -0.0, 0.0, //klm
-        -0.1, -0.1, //lmn
-        -0.4, 0.0, //mno
+        -0.0, 0.0, 0.0,     1,0,1,//klm
+        -0.1, -0.1,0.0,     1,0,1, //lmn
+        -0.4, 0.0, 0.0,     1,0,1,//mno
+        -0.3, -0.1, 0.0,    1,0,1,//mno
+
+        //out-end
+        -0.4, 0.0, 0.0,     0,0,1,//mno
+        -0.3, -0.1, 0.0,    0,0,1,//mno
+        -0.3, -0.1, 0.1,    0,0,1,//mno
+        -0.4, 0.0, 0.1,     0,0,1,//mno
+        -0.4, 0.0, 0.0,     0,0,1,//mno
+
+        // //in-bottom-side
+        -0.1, 0.0, 0.1,     0,0,1,//klm
+        -0.1, 0.0, 0.0,     0,0,1,//klm
+        -0.1, 0.1, 0.1,     0,0,1,//klm
+        -0.1, 0.1, 0.0,     0,0,1,//klm
+
+        // //in-middle-bottom
+        -0.3, 0.1, 0.1,     0,0,1,//klm
+        -0.3, 0.1, 0.0,     0,0,1,//klm
+
+        // //out-middle-corner
+        -0.4, 0.2, 0.0,     0,0,1,//klm
+        -0.4, 0.2, 0.1,     0,0,1,//klm
         
-        -0.3, -0.1, //mno
+        // //out-side-top
+        -0.4, 0.3, 0.0,     0,0,1,//klm
+        -0.4, 0.3, 0.1,     0,0,1,//klm
+
+        // //out-top-corner
+        -0.3, 0.4, 0.0,     0,0,1,//klm
+        -0.3, 0.4, 0.1,     0,0,1,//klm
+
+        // //out-top
+        -0.1, 0.4, 0.0,     0,0,1,//klm
+        -0.1, 0.4, 0.1,     0,0,1,//klm
+
+        // //top-end
+        -0.0, 0.3, 0.0,     0,0,1,//klm
+        -0.0, 0.3, 0.1,     0,0,1,//klm
+
+        // //in-top
+        -0.3, 0.3, 0.0,     0,0,1,//klm
+        -0.3, 0.3, 0.1,     0,0,1,//klm
+
+        // //in-top-side
+        -0.3, 0.2, 0.0,     0,0,1,//klm
+        -0.3, 0.2, 0.1,     0,0,1,//klm
+
+        // //in-middle-top
+        -0.1, 0.2, 0.0,     0,0,1,//klm
+        -0.1, 0.2, 0.1,     0,0,1,//klm
+
+        // //in-middle-corner
+        -0.0, 0.1, 0.0,     0,0,1,//klm
+        -0.0, 0.1, 0.1,     0,0,1,//klm
+
+        // //in-middle-side
+        -0.0, 0.0, 0.0,     0,0,1,//klm
+        -0.0, 0.0, 0.1,     0,0,1,//klm
+
+        // //in-bottom-corner
+        -0.1, -0.1,0.0,     0,0,1,//klm
+        -0.1, -0.1,0.1,     0,0,1,//klm
+
+        // //in-bottom-side
+        -0.3, -0.1,0.0,     0,0,1,//klm
+        -0.3, -0.1,0.1,     0,0,1,//klm
+
+        // //front-face
+        -0.3, -0.1,0.1,    1,0,1,//mno
+        -0.4, 0.0, 0.1,     1,0,1,//mno
+        -0.1, -0.1,0.1,     1,0,1, //lmn
+        -0.0, 0.0, 0.1,     1,0,1,//klm
+        -0.1, 0.0, 0.1,     1,0,1,//jkl
+        -0.0, 0.1, 0.1,     1,0,1,//ijk
+        -0.1, 0.1, 0.1,     1,0,1,//hij
+        -0.1, 0.2, 0.1,     1,0,1,//ghi
+        -0.3, 0.1, 0.1,     1,0,1,//fgh
+        -0.4, 0.2, 0.1,     1,0,1,//deg
+        -0.3, 0.2, 0.1,     1,0,1,//eff
+        -0.4, 0.3, 0.1,     1,0,1,//cde
+        -0.3, 0.3, 0.1,     1,0,1,//bcd
+        -0.3, 0.4, 0.1,     1,0,1,//c
+        -0.0, 0.3, 0.1,     1,0,1,//b
+        -0.1, 0.4, 0.1,     1,0,1,//a
     ];
-    // drawing(verticesS, 0, 16, gl.TRIANGLE_STRIP); 
+    const indicesS =[
+        0,1,2,3,4,5,6,7,8,9,10,11,12,
+        13,14,15,16,17,
+        18,19,20,21,
+        22,23,
+        24,25,
+        26,27,
+        28,29,
+        30,31,
+        32,33,
+        34,35,
+        36,37,
+        38,39,
+        40,41,
+        42,43,
+        44,45,
+        46,47,
+        48,49,
+        50,51,
+        52,53,54,55,56,57,58,59,60,61,62,63,64,65,66
+    ]
 
     const objects = [
         {
@@ -388,16 +567,20 @@ const main = () =>{
             length: 11,
             type: gl.LINE_LOOP
         },
-        // {
-        //     vertices: verticesU,
-        //     length: 10,
-        //     type: gl.TRIANGLE_STRIP,
-        // },
-        // {
-        //     vertices: verticesS,
-        //     length: 16,
-        //     type: gl.TRIANGLE_STRIP,
-        // },
+        {
+            name: 'U',
+            vertices: verticesU,
+            indices: indicesU,
+            length: 10,
+            type: gl.TRIANGLE_STRIP,
+        },
+        {
+            name: 'S',
+            vertices: verticesS,
+            indices: indicesS,
+            length: 16,
+            type: gl.TRIANGLE_STRIP,
+        },
     ]
     console.log(objects);
 
